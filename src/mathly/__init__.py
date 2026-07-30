a464cebc59438d5072ad8ef17c290c3f71e5a2f1
ZohaibSaeed1701 <zohaibsaeed1701@gmail.com>
2026-07-30 22:59:52 +0500

    deployment configrations

--- src/mathly/__init__.py
+++ src/mathly/__init__.py
@@ -6,5 +6,5 @@ from .algebra.number_line import NumberLine
 from .algebra.grid2d import Grid2D
 from .calculus.graph import CalculusGraph
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["MathBoard", "NumberLine", "Grid2D", "CalculusGraph"]
